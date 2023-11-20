19ae12cdb2635dd3cd0de8c9292fa4bc2a486ab3
Jynic <108580115+Jynic@users.noreply.github.com>
2023-11-20 14:14:47 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type='text/css' href="login.css">
+    <link rel="stylesheet" type='text/css' href="./dist/output.css">
+    <title>LOGIN</title>
+</head>
+<body>
+    <div id='container' class=' '>
+        <div id='content' class='float-left text-7xl mt-60 ml-40 text-violet-600 '>
+            <p class=''>Welcome To</p>
+            <p>EDUCONCEPT</p>
+        </div>
+
+        <div class='flex justify-end'>
+        <div id='aside' class='bg-violet-600 mr-12 mt-12 text-center'>
+            <img id='logo' class='block ml-auto mr-auto mt-10' src="img/logo-dummy.png" alt="logo-dummy">
+            <br>
+            <hr id='garis' class='mr-auto ml-auto mt-3 mb-10'>
+            <label class=' ' id='txtUser'>Username : <span class ='text-red-700'>*</span>&nbsp &nbsp
+            <input type="text" name='txtUsername' placeholder='  Type Your Username'>
+            </label>
+            <br><br>
+            <label class=' ' id='txtPass'>Password : <span class ='text-red-700'>*</span>&nbsp &nbsp
+            <input type="text" name='txtPassword' placeholder='  Type Your Password'>
+            </label>
+            <br>
+            <button id='btnSubmit' name='btnSubmit' class='mt-40 w-40 h-14 bg-white rounded-full'>Submit</button>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
